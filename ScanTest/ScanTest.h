5344311776ae27018ad75ea42c5b2b3c5679048d
Kevin Harwood <kevin.harwood@mutualmobile.com>
2016-02-22 11:27:52 -0600

    Initial Commit

---
+++ ScanTest/ScanTest.h
@@ -0,0 +1,19 @@
+//
+//  ScanTest.h
+//  ScanTest
+//
+//  Created by Kevin Harwood on 2/22/16.
+//  Copyright © 2016 Kevin Harwood. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ScanTest.
+FOUNDATION_EXPORT double ScanTestVersionNumber;
+
+//! Project version string for ScanTest.
+FOUNDATION_EXPORT const unsigned char ScanTestVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ScanTest/PublicHeader.h>
+
+
